6beda21891636f8a28e7bdc1cec790ca1dffd4f9
HunterArley <huangxiaolei9458@gmail.com>
2017-07-11 17:02:15 +0800

    去除多余的类

--- app/src/main/java/com/tengxin/bannerview/BannerViewPager.java
+++
@@ -1,8 +0,0 @@
-package com.tengxin.bannerview;
-
-/**
- * Created by hxl on 2017/7/6.
- */
-
-public class BannerViewPager {
-}
--- app/src/main/java/com/tengxin/bannerview/holder/BannerHolder.java
+++
@@ -1,8 +0,0 @@
-package com.tengxin.bannerview.holder;
-
-/**
- * Created by hxl on 2017/7/6.
- */
-
-public class BannerHolder {
-}
